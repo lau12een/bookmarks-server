6f43269c0b03b6bb8cdebc3f105bd3442f7b02f9
Laureen Cruz <laureencrz@outlook.com>
2019-07-22 12:41:23 -0700

    added restful API assignment requirements

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const bookmarksRouter = require('./bookmarks/bookmarks-router')
+const logger = require('./logger')
 
 const app = express()
 
@@ -13,7 +14,7 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 }))
 app.use(cors())
 app.use(helmet())
-app.use(bookmarksRouter)
+app.use('/api/bookmarks', bookmarksRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello, world!')
@@ -21,28 +22,52 @@ app.get('/', (req, res) => {
 
 //API key Middleware function
 
-app.use(function validateBearerToken(req, res, next) {
-  const apiToken = process.env.API_TOKEN
-  const authToken = req.get('Authorization')
+//app.use(function validateBearerToken(req, res, next) {
+//  const apiToken = process.env.API_TOKEN
+//  const authToken = req.get('Authorization')
+//
+//  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+//       logger.error(`Unauthorized request to path: ${req.path}`);
+//    return res.status(401).json({ error: 'Unauthorized request' })
+//  }
+//  // move to the next middleware
+//  next()
+//})
 
-  if (!authToken || authToken.split(' ')[1] !== apiToken) {
-       logger.error(`Unauthorized request to path: ${req.path}`);
+//implements new solution
+app.use(function validateBearerToken(req, res, next) {
+  const authToken = req.get('Authorization')
+  logger.error(`Unauthorized request to path: ${req.path}`)
+
+  if (!authToken || authToken.split(' ')[1] !== API_TOKEN) {
     return res.status(401).json({ error: 'Unauthorized request' })
   }
-  // move to the next middleware
+
   next()
 })
 
-//Error Handler
+//Error Handler adds solution code 
+//app.use(function errorHandler(error, req, res, next) {
+//   let response
+//   if (NODE_ENV === 'production') {
+//     response = { error: { message: 'server error' } }
+//   } else {
+//     console.error(error)
+//     response = { message: error.message, error }
+//   }
+//   res.status(500).json(response)
+// })
+
 app.use(function errorHandler(error, req, res, next) {
-   let response
-   if (NODE_ENV === 'production') {
-     response = { error: { message: 'server error' } }
-   } else {
-     console.error(error)
-     response = { message: error.message, error }
-   }
-   res.status(500).json(response)
- })
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } }
+  } else {
+    console.error(error)
+    logger.error(error.message)
+    response = { message: error.message, error }
+  }
+  res.status(500).json(response)
+})
 
 module.exports = app
